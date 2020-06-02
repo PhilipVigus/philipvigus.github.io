505a55b0040e66ac7b872d761897e04a379229be
Phil Vigus <philipvigus@yahoo.co.uk>
2020-06-02 09:03:35 +0100

    Update loading screen

--- js/loadingScreen.js
+++ js/loadingScreen.js
@@ -1,6 +1,14 @@
 export default function LoadingScreen() {
-  let typedText = document.getElementById('typed-text')
-  this.start = function startAnimating() {
-    typedText.innerHTML = "test"
+  const animationData = [ { text: "load 'Phils amazing portfolio'", timing: 10000 } ];
+  let typedText = document.getElementById('typed-text');
+
+  function doSingleAnimation(animation) {
+    setTimeout(() => { typedText.innerHTML += 'a'; }, 10000)
+  }
+
+  this.startAnimating = function startAnimating() {
+    animationData.forEach((animation) => {
+      doSingleAnimation(animation);
+    });
   };
 }
